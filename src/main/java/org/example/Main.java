bff0bcaed6941a37927c5475cd9dd7f62ffb8302
Caren <caren.arevalo-v@uniminuto.edu.co>
2025-02-19 11:53:36 -0500

    Comandos BD

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,5 +17,10 @@ public class Main {
         System.out.println("MULTIPLICAION");
         System.out.println("DIVISION");
 
+        System.out.println("DDL");
+        System.out.println("DML");
+        System.out.println("TCL");
+        System.out.println("COMANDOS BASES DE DATOS");
+
     }
 }
