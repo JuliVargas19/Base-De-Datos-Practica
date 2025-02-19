029814879c4460f7099df338ddbb0e555119cb37
Caren <caren.arevalo-v@uniminuto.edu.co>
2025-02-19 11:24:21 -0500

    Salidas por Consola

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Main class");
+        System.out.println("Estamo Aprendiendo Comandos Git");
+        System.out.println("Repasando Comandos");
     }
 }
